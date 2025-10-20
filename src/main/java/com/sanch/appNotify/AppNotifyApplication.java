3dad1cf8ff4294a8442596ee3b4870c059b969cf
sanch <sanchezbolanoscristian@gmail.com>
2025-10-20 17:45:30 -0500

    Proyecto inicial SpringBoot/ Spring initializr

---
+++ src/main/java/com/sanch/appNotify/AppNotifyApplication.java
@@ -0,0 +1,13 @@
+package com.sanch.appNotify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppNotifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppNotifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sanch/appNotify/AppNotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanch.appNotify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppNotifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
